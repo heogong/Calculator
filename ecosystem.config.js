d1bce00461fbe557d8d8cfa801da970cef41c841
DESKTOP-ASVRCK1\JJ <heogong79@gmail.com>
2020-10-16 16:17:39 +0900

    pm2 localhost -> 0.0.0.0

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,13 @@
 module.exports = {
 	apps: [
 		{
-			name: 'adApp',
+			name: 'CalApp',
 			exec_mode: 'cluster',
-			instances: 'max', // Or a number of instances
+			instances: '1', // Or a number of instances
 			script: './node_modules/nuxt/bin/nuxt.js',
+			env: {
+				HOST: '0.0.0.0',
+			},
 			args: 'start',
 		},
 	],
